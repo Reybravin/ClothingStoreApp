ef67852b867db6c67132c860b544f76132c5e3c1
Sergiy Sachuk <sergiysachuk@Sergiys-MacBook-Pro.local>
2017-11-03 21:58:23 +0200

    IBOutlets programatically

---
+++ coder-swag/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Sergiy Sachuk on 11/3/17.
+//  Copyright © 2017 Sergiy Sachuk. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
