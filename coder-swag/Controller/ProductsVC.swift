7721e2e4fdbb0c4cd7c58cd4725cd2a1cb84d2f9
Sergiy Sachuk <sergiysachuk@Sergiys-MacBook-Pro.local>
2017-11-06 10:27:38 +0200

    to fix seques to collection views

---
+++ coder-swag/Controller/ProductsVC.swift
@@ -0,0 +1,41 @@
+//
+//  ProductsVC.swift
+//  coder-swag
+//
+//  Created by Sergiy Sachuk on 11/6/17.
+//  Copyright © 2017 Sergiy Sachuk. All rights reserved.
+//
+
+import UIKit
+
+class ProductsVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    @IBOutlet weak var productsCollection: UICollectionView!
+
+    private(set) public var products = [Product]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        productsCollection.dataSource = self
+        productsCollection.delegate = self
+    }
+    
+    func initProducts(category: Category) {
+        products = DataService.instance.getProducts(forCategoryTitle: category.title)
+        navigationItem.title = category.title
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProductCell", for: indexPath) as? ProductCell{
+            let product = products[indexPath.row]
+            cell.updateViews(product: product)
+            return cell
+        }
+        return ProductCell()
+    }
+}
